9aad3a671bcb52391a993304e065dd20d167ac7d
Nelson Reina <nelson.reina@ibis-maganement.com>
2018-03-29 10:55:09 -0400

    render always to index.html when route is not found/ react router will handle route

--- src/server/app.js
+++ src/server/app.js
@@ -6,9 +6,11 @@ const cors = require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+const publicPath = path.resolve(__dirname, '../../public');
+
 const app = express();
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, '../../public')));
+app.use(express.static(publicPath));
 // Body Parser initi
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,6 +34,9 @@ app.get('/api/:key', async (req, res) => {
     console.error(error);
     res.send('Error occured on the server!');
   }
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(publicPath, 'index.html'));
 });
 
 app.listen(PORT, () => {
