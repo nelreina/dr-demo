1fe5291d5fc342558b36b81abd2a92f15db0d12f
Nelreina <nelreina@gmail.com>
2018-03-31 00:05:57 -0400

    changed reports to object with options and changed key for data

--- src/server/import-data-2-redis.js
+++ src/server/import-data-2-redis.js
@@ -1,26 +1,69 @@
 const { createClient } = require('then-redis');
 const fs = require('fs');
 const path = require('path');
-const { uniq } = require('lodash');
+const { find } = require('lodash');
 
 const periods = {
-  433: { id: 433, period: 201801, name: 'Jan 2018' },
+  // 433: { id: 433, period: 201801, name: 'Jan 2018' },
   436: { id: 436, period: 201802, name: 'Feb 2018' }
 };
+
+const reports = {
+  BS: {
+    code: 'BS',
+    name: 'Balance Sheet',
+    header: 'ResidentDetailed',
+    countAmountColumns: 4
+  },
+  PL: {
+    code: 'PL',
+    name: 'Profit & Loss',
+    header: 'Resident',
+    countAmountColumns: 2
+  },
+  CL: {
+    code: 'CL',
+    name: 'Contingent Liability',
+    header: 'ResidentDetailed',
+    countAmountColumns: 4
+  },
+  SUB1: {
+    code: 'SUB1',
+    name: 'Sub Report I',
+    header: 'Custom',
+    countAmountColumns: 12
+  },
+  SUB2: {
+    code: 'SUB2',
+    name: 'Sub Report II',
+    header: 'ResidentDetailed',
+    countAmountColumns: 4
+  }
+};
+const findReportCode = name => {
+  const rkeys = Object.keys(reports);
+  const rdata = rkeys.map(key => reports[key]);
+  const ret = find(rdata, { name });
+  return ret ? ret.code : null;
+};
+
 const dataDir = path.resolve(__dirname, '../../data');
 module.exports = async client => {
   client.flushdb();
   client.set('periods', JSON.stringify(periods));
-  const reports = [];
   fs.readdirSync(dataDir).forEach(function(file) {
-    const key = file.replace('.json', '');
-    console.log(`importing... ${key}`);
-    reports.push(key.substring(6));
-    const data = require(`${dataDir}/${file}`);
-    client.set(key, JSON.stringify(data));
+    console.log(`importing... ${file}`);
+    const name = file.replace('.json', '').substring(6);
+    // console.log(`name substr... ${name.substring(0, 3)}`);
+    const code = findReportCode(name);
+    if (code) {
+      const key = `${file.substring(0, 3)} - ${code}`;
+      const data = require(`${dataDir}/${file}`);
+      client.set(key, JSON.stringify(data));
+    }
     // console.info(data);
   });
-  client.set('reports', JSON.stringify(uniq(reports)));
+  client.set('reports', JSON.stringify(reports));
   const ret = await client.get('reports');
-  console.info('Imported reports: ', ret);
+  console.info('Imported reports: ', JSON.parse(ret));
 };
