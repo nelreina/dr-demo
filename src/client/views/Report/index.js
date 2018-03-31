59366ca84e045b507cb0d80e42e6dec7a956f012
Nelreina <nelreina@gmail.com>
2018-03-31 05:28:25 -0400

    handled shouldComponentUpdate for when refreshing the browser

--- src/client/views/Report/index.js
+++ src/client/views/Report/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { assign, reduce } from 'lodash';
+import { assign, reduce, isEqual } from 'lodash';
 import * as actions from '../../store/reducers/ncoa';
 import Row from './Row';
 import ReportHeader from './ReportHeader';
@@ -31,31 +31,25 @@ class Report extends Component {
   componentWillMount() {
     this.fetchNCOA(this.props);
   }
-  // shouldComponentUpdate = (nextProps, nextState) => {
-  //   console.info('should', 'this.props', this.props);
-  //   console.info('should', 'nextProps', nextProps);
-  //   const should =
-  //     (nextProps.periodId !== this.props.periodId &&
-  //       nextProps.data.length === 0) ||
-  //     this.props.data.length !== nextProps.data.length;
-  //   return should;
-  // };
-
-  componentDidUpdate() {
-    // this.fetchNCOA(this.props);
-  }
-  fetchNCOA = props => {
-    const { match: { params }, periodId, data } = props;
-    // console.info(params, periodId, data);
-    if (periodId) {
-      const id = `${periodId} - ${params.id}`;
-      props.fetchNcoa(id);
+  shouldComponentUpdate = (nextProps, nextState) => {
+    if (isEqual(nextProps.activePeriod, this.props.activePeriod)) {
+      if (nextProps.data) {
+        return true;
+      }
+      return false;
+    } else {
+      this.fetchNCOA(this.props);
     }
+    return true;
+  };
+
+  fetchNCOA = props => {
+    const { match: { params }, activePeriod } = props;
+    props.fetchNcoa(params.id);
   };
   render() {
     const { match: { params }, data, reports } = this.props;
-    const rows = data;
-    const report = reports[params.id];
+    const report = reports[params.id] || {};
     const cols = getColsArray(report);
     const options = { style, amountFormat, cols };
     return (
@@ -64,9 +58,10 @@ class Report extends Component {
         <table style={{ zoom: '70%' }} className="table table-sm">
           <ReportHeader options={{ header }} report={report} />
           <tbody>
-            {rows.map((row, key) => (
-              <Row key={key} row={row} options={options} />
-            ))}
+            {data &&
+              data.map((row, key) => (
+                <Row key={key} row={row} options={options} />
+              ))}
           </tbody>
         </table>
       </div>
@@ -74,8 +69,8 @@ class Report extends Component {
   }
 }
 const msp = state => ({
-  data: state.ncoa.data ? state.ncoa.data : [],
+  data: state.ncoa.data,
   reports: state.reports.data ? state.reports.data : {},
-  periodId: state.periods.activePeriod ? state.periods.activePeriod.id : null
+  activePeriod: state.periods.activePeriod
 });
 export default connect(msp, actions)(Report);
