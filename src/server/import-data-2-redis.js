4fe3a314d3604f8da8076c79fe1f1dd05b99dd6e
Nelreina <nelreina@gmail.com>
2018-04-02 03:23:13 -0400

    feature/load-translations: importTranslations and reorganized for reuse

--- src/server/import-data-2-redis.js
+++ src/server/import-data-2-redis.js
@@ -1,72 +1,19 @@
 const { createClient } = require('then-redis');
 const fs = require('fs');
 const path = require('path');
-const { find } = require('lodash');
-
+const { importReports, importTranslations, reports } = require('./lib');
 const periods = {
   // 433: { id: 433, period: 201801, name: 'Jan 2018' },
   436: { id: 436, period: 201802, name: 'Feb 2018' }
-};
-const en = {};
-const es = {};
-const reports = {
-  BS: {
-    code: 'BS',
-    name: 'Balance Sheet',
-    header: 'ResidentDetailed',
-    countAmountColumns: 4
-  },
-  PL: {
-    code: 'PL',
-    name: 'Profit & Loss',
-    header: 'Resident',
-    countAmountColumns: 2
-  },
-  CL: {
-    code: 'CL',
-    name: 'Contingent Liability',
-    header: 'ResidentDetailed',
-    countAmountColumns: 4
-  },
-  SUB1: {
-    code: 'SUB1',
-    name: 'Sub Report I',
-    header: 'Custom',
-    countAmountColumns: 12
-  },
-  SUB2: {
-    code: 'SUB2',
-    name: 'Sub Report II',
-    header: 'ResidentDetailed',
-    countAmountColumns: 4
-  }
-};
-const findReportCode = name => {
-  const rkeys = Object.keys(reports);
-  const rdata = rkeys.map(key => reports[key]);
-  const ret = find(rdata, { name });
-  return ret ? ret.code : null;
 };
 
 const dataDir = path.resolve(__dirname, '../../data');
 module.exports = async (client, logger) => {
   client.flushdb();
+  importReports(client, logger, dataDir);
+  await importTranslations(client, logger, dataDir, 'es.csv');
   client.set('periods', JSON.stringify(periods));
-  fs.readdirSync(`${dataDir}/reports`).forEach(function(file) {
-    logger.debug(`importing... ${file}`);
-    const name = file.replace('.json', '').substring(6);
-    // logger.info(`name substr... ${name.substring(0, 3)}`);
-    const code = findReportCode(name);
-    if (code) {
-      const key = `${file.substring(0, 3)} - ${code}`;
-      const data = require(`${dataDir}/reports/${file}`);
-      client.set(key, JSON.stringify(data));
-    }
-    // logger.info(data);
-  });
   client.set('reports', JSON.stringify(reports));
-  client.set('locale-es', JSON.stringify(es));
-  client.set('locale-en', JSON.stringify(en));
-  const ret = await client.get('reports');
+  const ret = await client.get('locale-es');
   logger.info('Imported reports: ', ret);
 };
---
+++ src/server/lib.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const { find } = require('lodash');
+const { converters } = require('nelreina-node-utils');
+const { toJSON } = converters;
+
+const findReportCode = name => {
+  const rkeys = Object.keys(reports);
+  const rdata = rkeys.map(key => reports[key]);
+  const ret = find(rdata, { name });
+  return ret ? ret.code : null;
+};
+const reports = {
+  BS: {
+    code: 'BS',
+    name: 'Balance Sheet',
+    header: 'ResidentDetailed',
+    countAmountColumns: 4
+  },
+  PL: {
+    code: 'PL',
+    name: 'Profit & Loss',
+    header: 'Resident',
+    countAmountColumns: 2
+  },
+  CL: {
+    code: 'CL',
+    name: 'Contingent Liability',
+    header: 'ResidentDetailed',
+    countAmountColumns: 4
+  },
+  SUB1: {
+    code: 'SUB1',
+    name: 'Sub Report I',
+    header: 'Custom',
+    countAmountColumns: 12
+  },
+  SUB2: {
+    code: 'SUB2',
+    name: 'Sub Report II',
+    header: 'ResidentDetailed',
+    countAmountColumns: 4
+  }
+};
+
+exports.importReports = (client, logger, dataDir) => {
+  fs.readdirSync(`${dataDir}/reports`).forEach(function(file) {
+    logger.debug(`importing... ${file}`);
+    const name = file.replace('.json', '').substring(6);
+    const code = findReportCode(name);
+    if (code) {
+      const key = `${file.substring(0, 3)} - ${code}`;
+      const data = require(`${dataDir}/reports/${file}`);
+      client.set(key, JSON.stringify(data));
+    }
+  });
+};
+exports.importTranslations = async (client, logger, dataDir, file) => {
+  logger.debug(`importing... ${file}`);
+  const name = file.replace('.csv', '');
+  const key = `locale-${name}`;
+  const data = fs
+    .readFileSync(`${dataDir}/translations/${file}`)
+    .toLocaleString();
+  const json = await toJSON(data);
+  const words = {};
+  json.forEach(word => {
+    words[word.key] = word.value.trim();
+  });
+  client.set(key, JSON.stringify(words));
+};
+exports.reports = reports;
