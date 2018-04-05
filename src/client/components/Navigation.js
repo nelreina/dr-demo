4d838dbeac7ff646cd9cc48e2cdb67303dbf0a4a
Nelreina <nelreina@gmail.com>
2018-04-04 20:18:16 -0400

    added navigation for admin users

---
+++ src/client/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { translate } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
+const Navigation = ({ t }) => {
+  return (
+    <ul className="nav">
+      <li className="nav nav-item">
+        <NavLink className="nav-link" to="/translate">
+          Translate
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default translate()(Navigation);
--- src/client/components/TopBar.js
+++ src/client/components/TopBar.js
@@ -3,12 +3,14 @@ import { translate } from 'react-i18next';
 
 import Languages from './Languages';
 import UserInfo from './UserInfo';
+import Navigation from './Navigation';
 
 const TopBar = ({ t, auth, logout }) => {
   const { isAuthenticated, user } = auth;
   return (
     <div className="top-bar">
       <Languages langs={['en', 'es']} />
+      {user && user.isAdmin && <Navigation />}
       {isAuthenticated && <UserInfo user={user} logout={logout} />}
     </div>
   );
