e714356c2e7ca825dfd6b1ca575cf1adf1a67599
Nelreina <nelreina@gmail.com>
2018-04-10 04:44:35 -0400

    List work iterates object too with isobject flag

--- src/client/components/List.js
+++ src/client/components/List.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const List = ({ iterator, of: Component, ...rest }) => {
-  return iterator.map((item, key) => (
-    <Component key={key} item={item} {...rest} />
-  ));
+const List = ({ iterator, of: Component, isobject, ...rest }) => {
+  if (isobject) {
+    const keys = Object.keys(iterator);
+    return keys.map((item, key) => (
+      <Component key={key} item={iterator[key]} {...rest} />
+    ));
+  } else {
+    return iterator.map((item, key) => (
+      <Component key={key} item={item} {...rest} />
+    ));
+  }
 };
 
 export default List;
