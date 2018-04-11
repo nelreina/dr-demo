ca672f123238a49b3741e8f04034ace1b88d7b86
Nelreina <nelreina@gmail.com>
2018-04-11 06:44:16 -0400

    feature/sum-reports:handled number format dasboarditem

--- src/client/views/DashboardItem.js
+++ src/client/views/DashboardItem.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { translate } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import accounting from 'accounting';
+
+import { amountFormat } from './NCOA/util';
 
 const DashboardItem = ({ item: report, path, t }) => {
   return (
@@ -10,10 +13,10 @@ const DashboardItem = ({ item: report, path, t }) => {
         {/* <pre>{JSON.stringify(report.mainGroups, null, 2)}</pre> */}
         {report.mainGroups.map(group => (
           <li key={group.groupValue}>
+            <span>{group.groupName}</span>
             <span>
-              {group.group} {group.groupName}
+              {accounting.formatMoney(group.groupAmount, amountFormat)}
             </span>
-            <span>{group.groupAmount}</span>
           </li>
         ))}
       </ul>
