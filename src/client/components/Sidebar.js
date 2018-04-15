2d0ce5be34a17bc127cd94001bea83ea192b8ff6
Nelreina <nelreina@gmail.com>
2018-04-15 03:44:47 -0400

    feature/logo: added gl logo to project

--- src/client/components/Sidebar.js
+++ src/client/components/Sidebar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { translate } from 'react-i18next';
-
+import logo from '../assets/greenlight.png';
 import SidebarNav from './SidebarNav';
 
 const Sidebar = ({ t, auth }) => {
   const { isAuthenticated } = auth;
   return (
     <aside className="sidebar">
-      <img src="logo" alt="Site Logo" />
+      <img src={logo} alt="Site Logo" />
       {isAuthenticated ? <SidebarNav /> : <nav />}
       <div className="container footer">
         <small>IBIS Management </small>
