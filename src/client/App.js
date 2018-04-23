6aa6da060bb392b216d321957449ae56784dc8fb
Nelreina <nelreina@gmail.com>
2018-04-23 08:39:48 -0400

    feature/nr-react-private-route: implemented

--- src/client/App.js
+++ src/client/App.js
@@ -6,12 +6,12 @@ import { translate } from 'react-i18next';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import { compose } from 'recompose';
+import { PrivateRoute } from 'nr-react-private-route';
 
 import * as reportsActions from './store/reducers/reports';
 import * as periodsActions from './store/reducers/periods';
 import * as bankloadActions from './store/reducers/bankload';
 
-import PrivateRoute from './PrivateRoute';
 import TopBar from './components/TopBar';
 import Sidebar from './components/Sidebar';
 import Corrections from './views/Corrections';
@@ -51,7 +51,8 @@ class App extends Component {
     const {
       t,
       logout,
-      auth: { isAuthenticated }
+      auth: { isAuthenticated },
+      auth
     } = this.props;
     return (
       <Route
@@ -70,20 +71,35 @@ class App extends Component {
                   <Switch location={location}>
                     {/* <Redirect exact from="/" to="/dashboard" /> */}
                     <Route path="/login" exact component={Login} />
-                    <PrivateRoute path="/" exact component={Dashboard} />
                     <PrivateRoute
+                      auth={auth}
+                      path="/"
+                      exact
+                      component={Dashboard}
+                    />
+                    <PrivateRoute
+                      auth={auth}
                       path="/corrections"
                       exact
                       component={Corrections}
                     />
-                    <PrivateRoute path="/reports/:id" component={NCOA} />
-                    <PrivateRoute path="/reportlist" component={ReportList} />
                     <PrivateRoute
+                      auth={auth}
+                      path="/reports/:id"
+                      component={NCOA}
+                    />
+                    <PrivateRoute
+                      auth={auth}
+                      path="/reportlist"
+                      component={ReportList}
+                    />
+                    <PrivateRoute
+                      auth={auth}
                       path="/translations"
                       exact
                       component={Translations}
                     />
-                    <PrivateRoute component={NotFound} />
+                    <PrivateRoute auth={auth} component={NotFound} />
                   </Switch>
                 </CSSTransition>
               </TransitionGroup>
--- src/client/views/Login/index.js
+++ src/client/views/Login/index.js
@@ -3,20 +3,23 @@ import { translate } from 'react-i18next';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { authActions as actions } from 'nelreina-web-utils';
+import { Login } from 'nr-react-private-route';
+
 import LoginForm from './LoginForm';
+
 // import * as actions from '../store/reducers/auth';
 
-class Login extends Component {
+class LoginView extends Component {
   render() {
     const { login, t, auth } = this.props;
-    return auth.isAuthenticated ? (
-      <Redirect to="/" />
-    ) : (
-      <LoginForm action={login} message={auth.message} />
+    return (
+      <Login auth={auth} {...this.props}>
+        <LoginForm action={login} message={auth.message} />
+      </Login>
     );
   }
 }
-const TranslateLogin = translate()(Login);
+const TranslateLogin = translate()(LoginView);
 const ConnectLogin = connect(state => ({ auth: state.auth }), actions)(
   TranslateLogin
 );
